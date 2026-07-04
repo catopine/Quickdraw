734b7d70367df182e80520eae991c7689b323189
catopine <catoporcupine@gmail.com>
2026-07-04 08:32:38 -0500

    Quick little update for master branch (#1)
    * various and sundry updates for QD
    
    * update cause i forgor
    
    * update to correctly sanitize links and put my name on stuff cause i'm maintaining it now
    
    * Give proper acknowledgement

--- src/ui.ts
+++ src/ui.ts
@@ -29,7 +29,10 @@ export function fillDefaultEmbassies (): void {
     'Leningrad Airfield',
     'The Brotherhood of Malice',
     'Plum Island',
-    'Suspicious'
+    'Suspicious',
+    'Phantom',
+    'Fuchswalde',
+    'The Crimson Empire'
   ]
   const ignoreEmbassiesField = document.getElementById('ignoreEmbassies')! as HTMLInputElement
   ignoreEmbassiesField.value = DEFAULT_EMBASSY_FILTERS.join(', ')
@@ -43,7 +46,9 @@ export function fillDefaultPhrases (): void {
     'https://discord.com/invite/brotherhoodofmalice',
     'https://ijaka.xyz/discord',
     'https://osiris.valthost.com/',
-    'https://www.forum.the-black-hawks.org/'
+    'https://www.forum.the-black-hawks.org/',
+    'Seized by PRAF in Operation Sputnik',
+    'https://discord.gg/XWvERyc'
   ]
 
   const ignorePhrasesField = document.getElementById('ignorePhrases')! as HTMLInputElement
